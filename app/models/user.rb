d58212b5dd304d4b9df7a41150078a25446ff648
Alex Butirskiy <butirskiy@gmail.com>
2016-01-09 18:14:26 +0200

    First working example

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,30 @@
 class User < ActiveRecord::Base
   belongs_to :addr, class_name: 'Address'
+
+puts 'HELLO!!!'
+
+  def addr
+    a = super
+
+    a.class.class_eval('alias :old_save :save')
+    a.instance_variable_set(:@_caller, self)
+
+    a.class.send(:define_method, :save) do
+      unless new_record?
+        new_record = self.class.new(self.attributes)
+        new_record.id = nil
+        new_record.updated_at = nil
+        retval = new_record.old_save
+
+        @_caller.addr = new_record
+        @_caller.save
+        new_record.attributes.each { |k, v| self.send("#{k}=", v) }
+        retval
+      else
+        super()
+      end
+    end
+
+    a
+  end
 end
