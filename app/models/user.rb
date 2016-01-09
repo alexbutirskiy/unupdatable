476f665e431259b7b334e45b20ee085ccff5055c
Alex Butirskiy <butirskiy@gmail.com>
2016-01-09 21:20:43 +0200

    Interception point moved to _update_record

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,30 +1,30 @@
 class User < ActiveRecord::Base
   belongs_to :addr, class_name: 'Address'
 
-puts 'HELLO!!!'
-
   def addr
-    a = super
+    addr_inst = super
 
-    a.class.class_eval('alias :old_save :save')
-    a.instance_variable_set(:@_caller, self)
+    addr_inst.instance_variable_set(:@_caller, self)
 
-    a.class.send(:define_method, :save) do
-      unless new_record?
-        new_record = self.class.new(self.attributes)
-        new_record.id = nil
-        new_record.updated_at = nil
-        retval = new_record.old_save
+    addr_inst.class.send(:define_method, :_update_record) do
+      return(super()) if changes.empty?
 
-        @_caller.addr = new_record
-        @_caller.save
-        new_record.attributes.each { |k, v| self.send("#{k}=", v) }
-        retval
-      else
-        super()
+      new_record = self.class.new(self.attributes)
+      new_record.id = nil
+      new_record.updated_at = nil
+
+      return(false) unless new_record.send(:_create_record)
+
+      @_caller.addr = new_record
+
+      unless @_caller.save == true
+        raise("Unable to save dependent model: #{_caller.errors.full_messages}")
       end
+
+      new_record.attributes.each { |k, v| self.send("#{k}=", v) }
+      true
     end
 
-    a
+    addr_inst
   end
 end
